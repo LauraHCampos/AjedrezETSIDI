dad51f280fa7d645ff2fedf2c0974aa705ebab58
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-19 17:28:43 +0200

    Update Caballo.cpp

--- src/Caballo.cpp
+++ src/Caballo.cpp
@@ -1,9 +1,26 @@
 #include "Caballo.h"
 #include <cmath>
 
-bool Caballo::movimientoValido(int destX, int destY, Pieza* tablero[5][5]) 
-{
-    int dx = std::abs(destX - x); //cambio en x
-    int dy = std::abs(destY - y); //cambio en y
-    return (dx == 2 && dy == 1) || (dx == 1 && dy == 2); //valido si se mueve en L en cualquier direccion
+bool Caballo::movimientoValido(int destX, int destY, Pieza* tablero[5][5]) {
+
+    if (tablero[destX][destY] && tablero[destX][destY]->getTipo() == "Rey")
+    {
+        return false; // no se puede capturar al rey
+    }
+
+    int dx = std::abs(destX - x);
+    int dy = std::abs(destY - y);
+
+    if ((dx == 2 && dy == 1) || (dx == 1 && dy == 2))
+        return true;
+
+    return false;
+}
+
+bool Caballo::puedeatacar(int destX, int destY, Pieza* tablero[5][5]) {
+    int dx = std::abs(destX - x);
+    int dy = std::abs(destY - y);
+
+    // Movimiento en L: dos en una dirección y uno en la otra
+    return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
 }
