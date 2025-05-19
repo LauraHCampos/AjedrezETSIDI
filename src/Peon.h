f401caaaaa1765f6d6d765521bb494b566a75fa7
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-19 17:30:00 +0200

    Update Peon.h

--- src/Peon.h
+++ src/Peon.h
@@ -3,10 +3,11 @@
 
 //se muven hacia delante o en diagonal SOLO para capturar
 class Peon : public Pieza //clase Peon derivada de Pieza
-{ 
+{
 public:
     Peon(int x, int y, bool color) : Pieza(x, y, color) {} //constructor con la posicion y el color
     bool movimientoValido(int destX, int destY, Pieza* tablero[5][5]); //llama a la funcion de movimeintos validos
-    string getTipo() { return "P"; } //imprime P en el tablero
-    
+    bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]); //comprueba si se puede comer al rey
+    string getTipo() { return "Peon"; } //imprime P en el tablero
+    int getValor() const { return 1; } //devuelve un valor de 1
 };
