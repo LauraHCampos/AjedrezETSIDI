6dc3d326e8f19c1147db0be0c957d88374503f4d
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-19 17:30:34 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -11,14 +11,18 @@ protected:
 public:
     Pieza(int x, int y, bool color) : x(x), y(y), color(color) {} //constructor generico
     virtual ~Pieza() {} // destructor generico
-    
+
     virtual bool movimientoValido(int destX, int destY, class Pieza* tablero[5][5]) = 0;//devuelve si el movimiento es valido
-    
+
+    //NUEVAFUNCION ademas de actualizar cada pieza
+    virtual bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]) = 0;//devuelve si la pieza puede atacar al rey para comprobar el jaque
+
     bool getColor() { return color; } //devuelve el color de la pieza
     void setPos(int nx, int ny) { x = nx; y = ny; } //define la posicion de la pieza
     int getX() { return x; } //devuelve la coordenada x de la pieza
     int getY() { return y; } //devuelve la coordenada y de la pieza
     virtual string getTipo() = 0; //devuelve el tipo de pieza (virtual puro)
+    virtual int getValor() const = 0; // devuelve el valor asignado a cada pieza (virtual puro)
 };
 
 
