eaf98bb15527b33c991c53d24f03e83665a7a4ef
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-19 17:32:20 +0200

    Update Rey.h

--- src/Rey.h
+++ src/Rey.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Pieza.h"
+#include "Tablero.h"
 
 //el rey se puede mover una casilla en cualquier direccion
 class Rey : public Pieza //clase Rey derivada de Pieza
@@ -7,6 +8,7 @@ class Rey : public Pieza //clase Rey derivada de Pieza
 public:
     Rey(int x, int y, bool color) : Pieza(x, y, color) {} //constructor especifico
     bool movimientoValido(int destX, int destY, Pieza* tablero[5][5]); //devuelve si el movimiento es valido
-    string getTipo() { return "R"; } //Devuelve una letra R para identificarlo
+    bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]); //comprueba si se puede comer al rey
+    string getTipo() { return "Rey"; } //Devuelve una letra R para identificarlo
+    int getValor() const { return 100; } //devuelve un valor de 100
 };
-
