75794411783506a3931a67258b66c9ada7e46a20
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-19 17:29:30 +0200

    Update Peon.cpp

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -1,15 +1,32 @@
 #include <cmath>
 #include "Peon.h"
 
-bool Peon::movimientoValido(int destX, int destY, Pieza* tablero[5][5]) 
-{
-    int direccion = color ? -1 : 1; //se mueven hacia arriba (blanco) abajo(negro)
 
-    if (destX == x + direccion && y == destY && tablero[destX][destY] == nullptr) { //si la casilla esta libre se mueve hacia delante
-        return true;
+bool Peon::movimientoValido(int destX, int destY, Pieza* tablero[5][5]) {
+    int dir = color ? -1 : 1;  // blancas suben, negras bajan
+    int dx = destX - x;
+    int dy = destY - y;
+    if (tablero[destX][destY] && tablero[destX][destY]->getTipo() == "Rey")
+    {
+        return false; // no se puede capturar al rey
     }
-    if (destX == x + direccion && std::abs(destY - y) == 1 && tablero[destX][destY] != nullptr) { //se mueve uno hacia delante y otro a la dch o izqd SOLO en caso de que haya opcion a capturar
+
+    // Movimiento recto hacia adelante
+    if (dy == 0 && dx == dir && tablero[destX][destY] == nullptr)
         return true;
+
+    // Captura en diagonal: solo si hay pieza y es del oponente
+    if (std::abs(dy) == 1 && dx == dir && tablero[destX][destY] != nullptr) {
+        return tablero[destX][destY]->getColor() != color;
     }
-    return false; //otros movimientos no son validos
+
+    return false;
+}
+
+
+
+bool Peon::puedeatacar(int destX, int destY, Pieza* tablero[5][5]) 
+{
+    int dir = color ? -1 : 1; // blanco sube, negro baja
+    return destX == x + dir && std::abs(destY - y) == 1;
 }
