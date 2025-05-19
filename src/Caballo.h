11d4529c810c3e57af3da697345fc186249c1816
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-19 17:29:06 +0200

    Update Caballo.h

--- src/Caballo.h
+++ src/Caballo.h
@@ -3,10 +3,11 @@
 
 //se mueve en L
 class Caballo : public Pieza //clase Caballo derivada de Pieza
-{ 
+{
 public:
     Caballo(int x, int y, bool color) : Pieza(x, y, color) {} //constructor especifico
     bool movimientoValido(int destX, int destY, Pieza* tablero[5][5]); //devuelve si el movimiento es valido
-    string getTipo() { return "C"; } //Devuelve una letra C para identificarlo
+    bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]); //comprueba si se puede comer al rey
+    string getTipo() { return "Caballo"; } //Devuelve una letra C para identificarlo
+    int getValor() const { return 5; } //devuelve un valor de 5
 };
-
