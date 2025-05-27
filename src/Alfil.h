810252390b7fd065f147a8014bb26a882251c231
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-27 20:17:57 +0200

    Update Alfil.h

--- src/Alfil.h
+++ src/Alfil.h
@@ -8,6 +8,6 @@ public:
     Alfil(int x, int y, bool color) : Pieza(x, y, color) {} //constructor posicion inicial y color
     bool movimientoValido(int destX, int destY, Pieza* tablero[5][5]); //se puede hacer ese movimiento?
     bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]); //comprueba si se puede comer al rey
-    string getTipo() { return "Alfil"; } //letra que imprime en el tablero
+    string getTipo() { return "Alfil"; } //devuelve el nombre de la pieza
     int getValor() const { return 5; } //devuelve un valor de 5
 };
