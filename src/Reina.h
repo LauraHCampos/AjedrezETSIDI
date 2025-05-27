3e9e1398f6e0f270fbe9cda43a7630afb88ba3c5
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-27 20:19:20 +0200

    Update Reina.h

--- src/Reina.h
+++ src/Reina.h
@@ -5,10 +5,10 @@
 class Reina : public Pieza //clase Reina derivada de Pieza
 {
 public:
-    Reina(int x, int y, bool color) : Pieza(x, y, color) {} //constructor espec�fico
+    Reina(int x, int y, bool color) : Pieza(x, y, color) {} //constructor especifico
     bool movimientoValido(int destX, int destY, Pieza* tablero[5][5]); //devuelve si el movimiento es valido
     bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]); //comprueba si se puede comer al rey
-    string getTipo() { return "Queen"; } //Devuelve una letra Q para identificarlo
+    string getTipo() { return "Queen"; } //devuelve el nombre de la pieza
     int getValor() const { return 9; } //devuelve un valor de 9
 };
 
