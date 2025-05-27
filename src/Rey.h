ab7c6654684782b783a7c0fc5bed1bb3d6784971
Laura <80282528+LauraHCampos@users.noreply.github.com>
2025-05-27 20:19:35 +0200

    Update Rey.h

--- src/Rey.h
+++ src/Rey.h
@@ -9,6 +9,6 @@ public:
     Rey(int x, int y, bool color) : Pieza(x, y, color) {} //constructor especifico
     bool movimientoValido(int destX, int destY, Pieza* tablero[5][5]); //devuelve si el movimiento es valido
     bool puedeatacar(int destX, int destY, class Pieza* tablero[5][5]); //comprueba si se puede comer al rey
-    string getTipo() { return "Rey"; } //Devuelve una letra R para identificarlo
+    string getTipo() { return "Rey"; } //devuelve el nombre de la pieza
     int getValor() const { return 100; } //devuelve un valor de 100
 };
