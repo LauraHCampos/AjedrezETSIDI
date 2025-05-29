1591d7840c64943f61ea74ce0da2e8a1eaaf84fc
alejandro-bonilla <80429961+alejandro-bonilla@users.noreply.github.com>
2025-05-29 12:07:01 +0200

    Update Tablero.h (para imágenes piezas tablero)

--- src/Tablero.h
+++ src/Tablero.h
@@ -31,10 +31,12 @@ public:
     ~Tablero(); //destructor
 
     void dibujar(int sel2); //dibujar el tablero y las piezas
+    void imprimirTextura(const std::string& ruta, int x, int y); //para imprimir las imagenes de las piezas
     EstadoJuego click(float x, float y, int sel1); //control del raton al hacer clic para el movimiento de las piezas
-
+    void pintarmovimiento(float r, float g, float b, int i, int j);
     bool estaEnJaque(bool turnoBlanco);
     bool esJaqueMate(bool turnoBlanco);
     int evaluarTablero();
     EstadoJuego moverIA();
 };
+
